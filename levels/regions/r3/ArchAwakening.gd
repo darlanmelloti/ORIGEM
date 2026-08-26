6e8ad3108e3cf0f513329e94eafdd8aeaf80958b
Dev3 <dev3@origem.local>
2026-08-26 08:40:03 +0000

    feat(r3): settle arch lateral rest

--- levels/regions/r3/ArchAwakening.gd
+++ levels/regions/r3/ArchAwakening.gd
@@ -29,6 +29,7 @@ const ARCH_LATERAL_SILENCE_SCRIPT: Script = preload("res://levels/regions/r3/Arc
 const ARCH_MARGIN_STILL_SCRIPT: Script = preload("res://levels/regions/r3/ArchMarginStill.gd")
 const ARCH_LATERAL_MATTE_SCRIPT: Script = preload("res://levels/regions/r3/ArchLateralMatte.gd")
 const ARCH_MARGIN_CALM_SCRIPT: Script = preload("res://levels/regions/r3/ArchMarginCalm.gd")
+const ARCH_LATERAL_REST_SCRIPT: Script = preload("res://levels/regions/r3/ArchLateralRest.gd")
 const SILENT_CADENCE_DURATION: float = 1.35
 const SILENT_CADENCE_BEATS: int = 3
 const SILENT_CADENCE_SCALE_DELTA: float = 0.12
@@ -80,6 +81,7 @@ func _ready() -> void:
 	ARCH_MARGIN_STILL_SCRIPT.call("install", get_parent() as Node3D)
 	ARCH_LATERAL_MATTE_SCRIPT.call("install", get_parent() as Node3D)
 	ARCH_MARGIN_CALM_SCRIPT.call("install", get_parent() as Node3D)
+	ARCH_LATERAL_REST_SCRIPT.call("install", get_parent() as Node3D)
 	_build_trigger()
 	_build_effects()
 	_apply_awakened_state()
