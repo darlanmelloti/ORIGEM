4d30739eb44022c6b8bb1aced56cbdf36171a713
Dev3 <dev3@origem.local>
2026-08-26 08:35:45 +0000

    feat(r3): settle arch margin calm

--- levels/regions/r3/ArchAwakening.gd
+++ levels/regions/r3/ArchAwakening.gd
@@ -28,6 +28,7 @@ const ARCH_MARGIN_QUIET_SCRIPT: Script = preload("res://levels/regions/r3/ArchMa
 const ARCH_LATERAL_SILENCE_SCRIPT: Script = preload("res://levels/regions/r3/ArchLateralSilence.gd")
 const ARCH_MARGIN_STILL_SCRIPT: Script = preload("res://levels/regions/r3/ArchMarginStill.gd")
 const ARCH_LATERAL_MATTE_SCRIPT: Script = preload("res://levels/regions/r3/ArchLateralMatte.gd")
+const ARCH_MARGIN_CALM_SCRIPT: Script = preload("res://levels/regions/r3/ArchMarginCalm.gd")
 const SILENT_CADENCE_DURATION: float = 1.35
 const SILENT_CADENCE_BEATS: int = 3
 const SILENT_CADENCE_SCALE_DELTA: float = 0.12
@@ -78,6 +79,7 @@ func _ready() -> void:
 	ARCH_LATERAL_SILENCE_SCRIPT.call("install", get_parent() as Node3D)
 	ARCH_MARGIN_STILL_SCRIPT.call("install", get_parent() as Node3D)
 	ARCH_LATERAL_MATTE_SCRIPT.call("install", get_parent() as Node3D)
+	ARCH_MARGIN_CALM_SCRIPT.call("install", get_parent() as Node3D)
 	_build_trigger()
 	_build_effects()
 	_apply_awakened_state()
