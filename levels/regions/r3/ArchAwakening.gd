9b5a6108c539f0918727e5ece3fdc40b09ed20a8
Dev3 <dev3@origem.local>
2026-08-26 05:01:25 +0000

    feat(r3): settle arch lateral velvet

--- levels/regions/r3/ArchAwakening.gd
+++ levels/regions/r3/ArchAwakening.gd
@@ -23,6 +23,7 @@ const ARCH_LATERAL_CALM_SCRIPT: Script = preload("res://levels/regions/r3/ArchLa
 const ARCH_MARGIN_REST_SCRIPT: Script = preload("res://levels/regions/r3/ArchMarginRest.gd")
 const ARCH_LATERAL_STILLNESS_SCRIPT: Script = preload("res://levels/regions/r3/ArchLateralStillness.gd")
 const ARCH_MARGIN_MATTE_SCRIPT: Script = preload("res://levels/regions/r3/ArchMarginMatte.gd")
+const ARCH_LATERAL_VELVET_SCRIPT: Script = preload("res://levels/regions/r3/ArchLateralVelvet.gd")
 const SILENT_CADENCE_DURATION: float = 1.35
 const SILENT_CADENCE_BEATS: int = 3
 const SILENT_CADENCE_SCALE_DELTA: float = 0.12
@@ -68,6 +69,7 @@ func _ready() -> void:
 	ARCH_MARGIN_REST_SCRIPT.call("install", get_parent() as Node3D)
 	ARCH_LATERAL_STILLNESS_SCRIPT.call("install", get_parent() as Node3D)
 	ARCH_MARGIN_MATTE_SCRIPT.call("install", get_parent() as Node3D)
+	ARCH_LATERAL_VELVET_SCRIPT.call("install", get_parent() as Node3D)
 	_build_trigger()
 	_build_effects()
 	_apply_awakened_state()
